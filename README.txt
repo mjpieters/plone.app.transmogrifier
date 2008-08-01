f9be2b90295923d6dce91115450926d809c7eb04
Martijn Pieters <mj@zopatista.com>
2008-08-01 20:57:37 +0000

    Refactoring time: Move all Plone-specific blueprints to a new package, aptly named plone.transmogrifier. This lowers the collective.transmogrifier dependencies to just Zope2 and CMFCore.
     - browserdefault, criteria, workflowupdater, atschemaupdater and
       portaltransforms all now live in plone.transmogrifier
    
     - Update tests to use the new registrations
    
     - Remove plone from the collective.transmogrifier test buildout, replacing it
       with simply Zope and CMF 2.1.1
    
     - New buildout for plone.transmogrifier uses a collective.transmogrifier dev egg
    
     - Move over appropriate TODOs.

---
+++ README.txt
@@ -0,0 +1,27 @@
+*******************************
+Plone transmogrifier blueprints
+*******************************
+
+.. contents::
+
+This package contains several blueprints for collective.transmogrifier
+pipelines, commonly used to import content into a Plone site.
+
+Installation
+************
+
+See docs/INSTALL.txt for installation instructions.
+
+Credits
+*******
+
+Development sponsored by
+    Elkjøp Nordic AS
+    
+Design and development
+    `Martijn Pieters`_ at Jarn_
+    `Florian Schulze`_ at Jarn_
+    
+.. _Martijn Pieters: mailto:mj@jarn.com
+.. _Florian Schulze: mailto:fschulze@jarn.com
+.. _Jarn: http://www.jarn.com/
---
+++ setup.py
@@ -0,0 +1,59 @@
+version = '0.1'
+
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = ('\n'.join((
+    read('README.txt'), ''
+    'Detailed Documentation',
+    '**********************', '',
+    read('plone', 'transmogrifier', 'atschemaupdater.txt'), '',
+    read('plone', 'transmogrifier', 'workflowupdater.txt'), '',
+    read('plone', 'transmogrifier', 'browserdefault.txt'), '',
+    read('plone', 'transmogrifier', 'criteria.txt'), '',
+    read('plone', 'transmogrifier', 'portaltransforms.txt'), '',
+
+    read('docs', 'HISTORY.txt'), '',
+
+    'Download',
+    '********', ''
+)))
+    
+open('doc.txt', 'w').write(long_description)
+
+name='plone.transmogrifier'
+setup(name=name,
+      version=version,
+      description="Plone blueprints for collective.transmogrifier pipelines",
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='content import filtering plone',
+      author='Jarn',
+      author_email='info@jarn.com',
+      url='http://pypi.python.org/pypi/plone.transmogrifier',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.transmogrifier',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
