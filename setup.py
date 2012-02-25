2a312bc7ee5479c36a7cdbcd3af951994c6fb211
Martijn Pieters <mj@zopatista.com>
2012-02-25 22:20:25 +0100

    Move to .rst for GitHub.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 long_description = ('\n'.join((
-    read('README.txt'), ''
+    read('README.rst'), ''
     'Detailed Documentation',
     '**********************', '',
     read('src', 'plone', 'app', 'transmogrifier', 'atschemaupdater.txt'), '',
